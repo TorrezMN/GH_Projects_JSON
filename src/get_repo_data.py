e89250e20452d7c22432ef0a30669440ad7b2ca4
TorrezMN <torrez.mn@gmail.com>
2023-01-03 10:08:38 -0300

    SCRIPT done.

--- src/get_repo_data.py
+++ src/get_repo_data.py
@@ -5,54 +5,77 @@
 import json
 import requests
 
-repos_url = "https://api.github.com/users/TorrezMN/repos"
 
 REPO_LANG = None
 
 
 repos = []
+lista_completa = []
 
 
 def get_branches(rep):
     """Gets the list of branches of a repo."""
+    repo_branches = []
     branches_url = f"https://api.github.com/repos/TorrezMN/{rep}/branches"
-    branches_data = requests.get(branches_url)
-    return json.loads(branches_data.text)
+    branches_data = json.loads(requests.get(branches_url).text)
+
+    for k, v in enumerate(branches_data):
+        repo_branches.append(v["name"])
+
+    return repo_branches
 
 
 def get_langs(rep):
     """Gets a list of languagues used in the repo."""
     languages_url = f"https://api.github.com/repos/TorrezMN/{rep}/languages"
-    langs_data = requests.get(languages_url)
-    return json.loads(langs_data.text)
-
-
-def save_data():
-    with open("repos_data.json", "w", encoding="utf-8") as f:
-        json.dump(new_data, f, ensure_ascii=False, indent=4)
+    langs_data = json.loads(requests.get(languages_url).text)
+    if len(langs_data) > 0:
+        return [i for i in langs_data.keys()]
+    else:
+        return None
 
 
 def get_repos():
     """Gets a full list of my repos."""
-
-    data = requests.get(repos_url)
+    username = "torrez.mn@gmail.com"
+    token = "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"
+    repos_url = "https://api.github.com/users/TorrezMN/repos"
+    data = requests.get(repos_url, auth=(username, token))
 
     global new_data
 
     new_data = json.loads(data.text)
 
-    for i in new_data:
-        if not i["fork"]:
-            repos.append(i)
+    for i, v in enumerate(new_data):
+        if not v["fork"]:
+            repos.append(v)
 
 
+#  GETTING REPOS DATA
 get_repos()
 
 
+#  LOADING EXTRA DATA
 for i in repos:
-    print("NOMBRE -> ", i["name"])
-    print("URL -> ", i["html_url"])
-    print("DESCRIPCION : ", i["description"])
-    print("FORK : ", i["fork"])
-    print("URL : ", i["url"])
-    print("ESTRELLAS : ", i["stargazers_count"])
+    branches = get_branches(i["name"])
+    languages = get_langs(i["name"])
+
+    lista_completa.append(
+        {
+            "nombre": i["name"],
+            "data": {
+                "url": i["html_url"],
+                "DESCRIPCION": i["description"],
+                "FORK": i["fork"],
+                "URL": i["url"],
+                "ESTRELLAS": i["stargazers_count"],
+                "branches": branches,
+                "languages": languages,
+            },
+        }
+    )
+
+
+#  SAVE DATA TO JSON FILE
+with open("repos_data.json", "w", encoding="utf-8") as f:
+    json.dump(lista_completa, f, ensure_ascii=False, indent=4)
